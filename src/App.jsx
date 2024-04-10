159c3c08d589f88b6d7a26b37da7a1c50a166e6c
Bitew904 <bict1626@gmail.com>
2024-04-10 13:00:38 -0700

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+
 
 export default function App() {
   return (
-    <h1>hello</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
